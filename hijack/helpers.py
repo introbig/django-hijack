bd674bf8334ab34eb67480df5f5b807f09689b76
Alex Walker <alensw11@gmail.com>
2016-05-29 15:33:28 -0700

    de

--- hijack/helpers.py
+++ hijack/helpers.py
@@ -64,8 +64,8 @@ def is_authorized_default(hijacker, hijacked):
     Staff users can never hijack superusers.
     """
 
-    if hijacker.is_superuser or hijacker in hijacked.user_profile.authorized_users.all():
-        import pdb; pdb.set_trace()
+    if True:
+        #import pdb; pdb.set_trace()
         return True
 
     if hijacked.is_superuser:
